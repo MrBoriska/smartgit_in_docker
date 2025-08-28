226fe49855fffb0b6a6a8def843f9930f2824c19
Boris Lapin <mail@borislap.ru>
2025-08-28 09:33:47 +0300

    Updated script for get download link

--- get_download_link.py
+++ get_download_link.py
@@ -5,5 +5,5 @@ from bs4 import BeautifulSoup
 url = 'https://www.syntevo.com/smartgit/download/'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
-el = soup.find('a', class_='download-link', attrs={"data-platform":"Linux"})
+el = soup.find('a', class_='stretched-link', attrs={"title":"Debian Bundle: Download for Linux"})
 print(el.attrs["href"], end="")
