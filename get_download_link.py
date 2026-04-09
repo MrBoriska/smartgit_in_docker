8fdd96334a40fab1bd8b67287581a3945cf15846
Boris Lapin <mail@borislap.ru>
2026-04-09 16:01:22 +0000

    Fixed download link parser

--- get_download_link.py
+++ get_download_link.py
@@ -2,8 +2,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.syntevo.com/smartgit/download/'
+url = 'https://www.smartgit.dev/download/'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
-el = soup.find('a', class_='stretched-link', attrs={"title":"Debian Bundle: Download for Linux"})
+el = soup.find('a', attrs={"title":"Debian Bundle: Download for Linux"})
 print(el.attrs["href"], end="")
